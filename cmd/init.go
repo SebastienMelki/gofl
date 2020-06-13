10b11a7175ff8f7668272a9894c59dbc97aabded
Sebastien Melki <sebastien@anghami.com>
2020-06-13 09:24:04 +0300

    added bare bones init command that creates api, mobile and protos directory

---
+++ cmd/init.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init [APP_NAME]",
+	Short: "Create new gofl project",
+	Args:    cobra.MinimumNArgs(1),
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := os.Mkdir("mobile", os.ModePerm)
+		fmt.Println("Created mobile directory")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		err = os.Mkdir("api", os.ModePerm)
+		fmt.Println("Created api directory")
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		err = os.Mkdir("protos", os.ModePerm)
+		fmt.Println("Created protos directory")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
